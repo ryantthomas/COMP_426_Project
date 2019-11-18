ea68f3224cd5a3c9e590736718757d60435976a5
Erin Lyons <eglyons@live.unc.edu>
2019-11-18 18:20:12 -0500

    sligtly prettier

--- COMP426Project/searchtool.js
+++ COMP426Project/searchtool.js
@@ -8,12 +8,13 @@ export const onSearchClick = async function (event){
     let searchText = $('.input').val();
     let songData = await searchRequest(searchText);
     let i;
-    let htmlString = `<div class = "results"> <h1> Recommended based on your search: </h1>`
+    let htmlString = `<div id = "results"> <h1 class = "title is-4"> Recommended based on your search: </h1>`
     for (i=0; i < songData.Similar.Results.length; i++){
-        htmlString += `<p> ${songData.Similar.Results[i].Name} </p>`;
+        htmlString += `<div class = "column">  <div class = "box"> <p> ${songData.Similar.Results[i].Name} </p> 
+            <small> ${songData.Similar.Results[i].Type} </small> </div> </div>`;
     };
-    htmlString += `</div>`;
-    $('.results').replaceWith(htmlString);
+    //htmlString += `</div>`;
+    $('#results').replaceWith(htmlString);
 };
 
 export const searchRequest = async function (searchText){
@@ -22,10 +23,12 @@ export const searchRequest = async function (searchText){
         url: 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar',
         params: {
             q: searchText,
-            k: '348775-GroupPro-QFR7GQLZ'
+            k: '348775-GroupPro-QFR7GQLZ',
+            type: 'music'
         }
     });
     return result.data;
-}
+};
+
 
 
