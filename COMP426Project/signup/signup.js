9d54c7981b0867f7403884e77d3b72523532012c
Joshua Tabor <shua@email.unc.edu>
2019-11-18 20:43:05 -0500

    Working on the ajax post for login

--- COMP426Project/signup/signup.js
+++ COMP426Project/signup/signup.js
@@ -5,7 +5,21 @@ $(function () {
 async function addToBackEnd(event) {
     event.preventDefault();
 
-    // $.ajax({
+    // Include check to make sure some username and password have been entered
+    // if() {
 
+    // }
+
+    // $.ajax({
+    //     url: 'http://localhost:3000/account/create',
+    //     method: 'post',
+    //     name: $('usernameinput').val(),
+    //     pass: $('passwordinput').val(),
+    //     data: {
+    //         //anything we add here is optional
+    //         fullName: $('.fullnameinput').val(),
+    //         email: $('emailinput').val(),
+    //         age: $('.ageinput').val(),
+    //     },
     // });
 }
