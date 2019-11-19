0926341646c7cfa8d2832bd4fce4102878b75f08
Erin Lyons <eglyons@live.unc.edu>
2019-11-18 20:16:13 -0500

    add wiki links

--- COMP426Project/search/searchtool.js
+++ COMP426Project/search/searchtool.js
@@ -10,15 +10,13 @@ export const onSearchClick = async function (event){
     let searchDiv = $(event.target).closest(".search-div");
     searchButton.replaceWith(`<button id="search-button" class="button is-loading is-primary search">Loading</button>`); 
     let songData = await searchRequest(searchText);
-    
     //For loop adding boxes of songs
     let i;
     let htmlString = `<div id = "results"> <h1 class = "title is-4"> Recommended based on your search: </h1>`
     for (i=0; i < songData.Similar.Results.length; i++){
         htmlString += `<div class = "column">  <div class = "box"> <p> ${songData.Similar.Results[i].Name} </p> 
-            <small> ${songData.Similar.Results[i].Type} </small> </div> </div>`;
+            <a href = ${songData.Similar.Results[i].wUrl}> Wikipedia </a> </div> </div>`;
     };
-    
     $('#results').replaceWith(htmlString);
     searchDiv.replaceWith(`<div class="columns search-div">
         <input type="text" class="input" placeholder="Search specific artists or songs" required>
@@ -33,7 +31,8 @@ export const searchRequest = async function (searchText){
         params: {
             q: searchText,
             k: '348775-GroupPro-QFR7GQLZ',
-            type: 'music'
+            type: 'music',
+            info: 1
         }
     });
     return result.data;
