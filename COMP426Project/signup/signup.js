8de636ceccd8dfdf20bad106b4d38299a3bf7d40
Joshua Tabor <shua@email.unc.edu>
2019-11-21 02:58:13 -0500

    Got rid of comments

--- COMP426Project/signup/signup.js
+++ COMP426Project/signup/signup.js
@@ -20,23 +20,6 @@ async function createLogin (username, password, emailParam, full) {
             },
         },
     });
-    // Include check to make sure some username and password have been entered
-    // if() {
-
-    // }
-
-    // $.ajax({
-    //     url: 'http://localhost:3000/account/create',
-    //     method: 'post',
-    //     name: $('usernameinput').val(),
-    //     pass: $('passwordinput').val(),
-    //     data: {
-    //         //anything we add here is optional
-    //         fullName: $('.fullnameinput').val(),
-    //         email: $('emailinput').val(),
-    //         age: $('.ageinput').val(),
-    //     },
-    // });
 }
 
 async function addToBackEnd(event) {
