36fe3991cc8b606d6349da71592258ac353adcc6
Ryan Thomas <ryantthomas@users.noreply.github.com>
2019-12-05 13:49:43 -0500

    Fixed updating CRUD for favorites page

--- COMP426Project/favorites/favorites.js
+++ COMP426Project/favorites/favorites.js
@@ -22,6 +22,9 @@ const onSubmit = async function(event) {
         comments: comment
     }
     await postSuggestion(object, id);
+    $('#root').replaceWith(`<div id="root">
+                            </div>`);
+    renderFavorites();
 };
 
 const onDelete = async function(event) {
@@ -50,7 +53,8 @@ const onEdit = async function(event) {
     let id = suggestion.data("id");
     let commentChange = document.getElementById("edit-text").value
     await editSuggestion(id, commentChange);
-
+    let newSuggestion = await updateSuggestion(id);
+    suggestion.replaceWith(newSuggestion);
   }
 
 async function editSuggestion(id, commentChange){
@@ -153,7 +157,37 @@ let callAccountInfo = async function(jwt) {
     return response.data;
 };
 
-//
+
+async function updateSuggestion(id) {
+    data = await getFavoritesAtID(id);
+    
+    //same as the old
+    let token = window.localStorage.getItem('jwt');
+    let userData = await callAccountInfo(token);
+    let username = userData.user.name;
+    let htmlString = ``;   
+
+    if(username != data.result.username) {
+            htmlString = `<div class = "box suggestion-box" data-id= ${data.result.id}>
+                                <label class="label">User: </label>${data.result.username}
+                                <label class="label">Name: </label>${data.result.name} 
+                                <label class="label">Artist: </label>${data.result.artist}
+                                <label class="label">Reason for sharing: </label>${data.result.comments}
+                            </div>`+htmlString;
+    } else {
+        htmlString = `<div class = "box suggestion-box" data-id= ${data.result.id}>
+                            <label class="label">User: </label>${data.result.username}<br><br>
+                            <label class="label">Song: </label><i>${data.result.name}</i>
+                            <br> by ${data.result.artist}<br><br>
+                            <label class="label">Reason for sharing: </label>${data.result.comments}
+                            <br><br>
+                            <button class="button is-large is-info edit">Edit Reason</button>
+                            <button class="button is-large is-info delete-btn">Delete Suggestion</button>
+                        </div>`+htmlString;
+    }
+    return htmlString;
+}
+
 $(function(){
     renderFavorites();
     $('#main').on('click', '.submit', onSubmit);
@@ -161,3 +195,4 @@ $(function(){
     $('#main').on('click', '.edit', onEdit);
     $('#main').on('click', '.submit-edit', onEditSubmit);
 });
+
