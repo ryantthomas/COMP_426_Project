3d97d5633288c53e0128b979b666cdb8aec85c10
Erin Lyons <eglyons@live.unc.edu>
2019-12-08 23:41:55 -0500

    fix login error

--- COMP426Project/login/login.js
+++ COMP426Project/login/login.js
@@ -9,9 +9,9 @@ async function onLogin(event){
     event.preventDefault();
     let username = $('#userinput').val();
     let pass = $('#passinput').val();
-    let jwt = await loginCall(username, pass);
+    await loginCall(username, pass);
     //set the jwt to a cookie so it can be retrieved later
-    window.localStorage.setItem('jwt', jwt);
+    //window.localStorage.setItem('jwt', jwt);
     //to access jwt again use window.localStorage.getItem('jwt');
 };
 
@@ -30,9 +30,8 @@ async function loginCall(username, pass){
             $('#message').append('<div class = "notification is-success">' +
             '<button class = "delete deleteButton"></button>Successfully logged in. Redirecting in 3 seconds or click to return to the ' + 
             '<a href  = "../index.html">homepage' + '</div>');
-
+            window.localStorage.setItem('jwt', response.data.jwt);
             setTimeout(function() {window.location.replace("../index.html");}, 3000);
-            return response.data.jwt;
         }
     ).catch(err => {
         // alert("Error " + err.response.status);
